0f45adc3248774cc941514b7e28d6598c0068625
sumedha <sumedhanarayanan@gmail.com>
2019-02-23 02:46:39 +0000

    complete product rating feature functions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,19 @@
 class ReviewsController < ApplicationController
+
+  before_filter :authorize
   def create
-    product = Product.find_by_id(params[:product_id])
-    review = product.reviews.build(review_params)
+    @product = Product.find_by_id(params[:product_id])
+    review = @product.reviews.build(review_params)
     review.user = current_user
     review.save
-    redirect_to product
+    redirect_to @product
+  end
+
+  def destroy
+    review = Review.find_by_id(params[:id])
+    @product = Product.find_by_id(params[:product_id])
+    review.destroy
+    redirect_to @product
   end
 
   private
