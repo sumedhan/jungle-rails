27c7b6e2cb80e293a9a5d14f265e98bbd97ebb0f
sumedha <sumedhanarayanan@gmail.com>
2019-02-25 03:42:42 +0000

    add styling for log in page errors

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,19 @@
 class SessionsController < ApplicationController
   def new
+    user = User.new
   end
 
   def create
-    user = User.find_by_email(params['user'][:email])
+    user = User.find_by_email(params[:email])
       # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params['user'][:password])
+    if user && user.authenticate(params[:password])
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to '/'
     else
     # If user's login doesn't work, send them back to the login form.
-      redirect_to '/sessions/new'
+      redirect_to new_session_path, :flash => {:error => "Incorrect email or password, please try again!"}
     end
   end
 
